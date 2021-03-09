dafe4a14d45ee5c1d75a286b1c7418c70fe6648f
joa0neves <unfit4winter@gmail.com>
2021-03-09 18:31:37 +0000

    Update Cliente.java

--- Projeto/src/projeto/Cliente.java
+++ Projeto/src/projeto/Cliente.java
@@ -6,10 +6,27 @@
 package projeto;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.*;
+
 /**
  *
  * @author Joao
  */
 public class Cliente implements Serializable{
+    
+    @Column(name = "CLIENTE_ID")
+    private Integer cliente_id;
+    @Column(name = "USERNAME")
+    private String username;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "NIF")
+    private float NIF;
+    @Column(name = "DATA_NASCIMENTO")
+    private DateTime data_nascimento;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente")
     
 }
