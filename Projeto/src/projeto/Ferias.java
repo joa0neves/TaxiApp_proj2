a3ffc0a7cdf272b92e7871bccfa2d4629d2e3dd2
VascoCardos <78988922+VascoCardos@users.noreply.github.com>
2021-03-15 11:28:57 +0000

    Ferias

---
+++ Projeto/src/projeto/Ferias.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+import java.io.Serializable;
+/**
+ *
+ * @author Joao
+ */
+public class Ferias implements Serializable{
+
+}
